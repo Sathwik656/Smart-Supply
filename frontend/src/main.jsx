629fe9e57ca714312529d3c764298890e23e2fef
Sathwik <112880525+Sathwik656@users.noreply.github.com>
2026-04-28 19:02:04 +0530

    revert

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { BrowserRouter } from 'react-router-dom'   // ✅ ADD THIS
 import App from './App.jsx'
 import './index.css'
 
@@ -10,9 +9,7 @@ const queryClient = new QueryClient()
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <BrowserRouter>   {/* ✅ WRAP APP */}
-        <App />
-      </BrowserRouter>
+      <App />
     </QueryClientProvider>
   </React.StrictMode>,
 )
